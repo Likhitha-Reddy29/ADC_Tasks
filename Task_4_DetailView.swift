95ffdfe6310f2b65d7cc0bb7a304dec8aff15085
Likhitha-Reddy29 <146608404+Likhitha-Reddy29@users.noreply.github.com>
2024-06-18 22:41:03 +0530

    Add files via upload

---
+++ Task_4_DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  HelloApp
+//
+//  Created by Likhitha on 18/06/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail View")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
+
