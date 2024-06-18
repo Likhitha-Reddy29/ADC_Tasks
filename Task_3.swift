9d9d69fdd50d23e55418b9423c30e0fd780149bc
Likhitha-Reddy29 <146608404+Likhitha-Reddy29@users.noreply.github.com>
2024-06-18 22:39:12 +0530

    Add files via upload

---
+++ Task_3.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  HelloApp
+//
+//  Created by Likhitha on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4"]
+
+    var body: some View {
+        NavigationView {
+            List(items, id: \.self) { item in
+                Text(item)
+            }
+            .navigationTitle("Items List")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
